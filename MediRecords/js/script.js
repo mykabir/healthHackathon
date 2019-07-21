bf2e93fd2254e0df9085328d67dce253bdb1a2b8
Md. Yasin Kabir <mkdv6@mst.edu>
2019-07-20 19:57:20 -0500

    Uploading Main Frontend

---
+++ MediRecords/js/script.js
@@ -0,0 +1,56 @@
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+function signupSubmit(){
+  var signupData = JSON.stringify($("#signupForm").serializeArray());
+  console.log(signupData);
+  $.ajax({
+    type: "POST",
+    url: "signup",
+    data: signupData,
+    success: function(){},
+    dataType: "json",
+    contentType : "application/json"
+  });
+}
